e7605435d37a8284b2d4609f5c688d91a6c404a8
Jason Swett <jason@benfranklinlabs.com>
2018-10-30 14:39:33 -0400

    Delete unused episode CRUD functionality.

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,5 +1,5 @@
 class EpisodesController < ApplicationController
-  before_action :set_episode, only: [:show, :edit, :update, :destroy]
+  before_action :set_episode, only: [:show]
 
   # GET /episodes
   # GET /episodes.json
@@ -12,55 +12,7 @@ class EpisodesController < ApplicationController
   def show
   end
 
-  # GET /episodes/new
-  def new
-    @episode = Episode.new
-  end
-
-  # GET /episodes/1/edit
-  def edit
-  end
-
   # POST /episodes
-  # POST /episodes.json
-  def create
-    @episode = Episode.new(episode_params)
-
-    respond_to do |format|
-      if @episode.save
-        format.html { redirect_to @episode, notice: 'Episode was successfully created.' }
-        format.json { render :show, status: :created, location: @episode }
-      else
-        format.html { render :new }
-        format.json { render json: @episode.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /episodes/1
-  # PATCH/PUT /episodes/1.json
-  def update
-    respond_to do |format|
-      if @episode.update(episode_params)
-        format.html { redirect_to @episode, notice: 'Episode was successfully updated.' }
-        format.json { render :show, status: :ok, location: @episode }
-      else
-        format.html { render :edit }
-        format.json { render json: @episode.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /episodes/1
-  # DELETE /episodes/1.json
-  def destroy
-    @episode.destroy
-    respond_to do |format|
-      format.html { redirect_to episodes_url, notice: 'Episode was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_episode
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :episodes
+  resources :episodes, only: [:index, :show]
   resources :shows
   devise_for :users
   resources :people
