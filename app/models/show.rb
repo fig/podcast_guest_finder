4e48ed8e3f79c1485fdaf309337ad64c39e7e092
Jason Swett <jason@benfranklinlabs.com>
2018-10-30 14:29:28 -0400

    Validate XML feel URL format.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,6 @@
 class Show < ApplicationRecord
   validates :name, presence: true
-  validates :xml_feed_url, presence: true
+  validates :xml_feed_url,
+    presence: true,
+    format: URI::regexp(%w(http https))
 end
--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -5,4 +5,26 @@ RSpec.describe Show, type: :model do
 
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:xml_feed_url) }
+
+  describe 'xml_feed_url validity' do
+    describe 'invalid URL' do
+      before do
+        subject.xml_feed_url = 'blah'
+        subject.save
+      end
+
+      it 'is not valid' do
+        expect(subject.errors[:xml_feed_url]).to include('is invalid')
+      end
+    end
+
+    describe 'valid URL' do
+      before do
+        subject.xml_feed_url = 'https://rss.simplecast.com/podcasts/5739/rss'
+        subject.save
+      end
+
+      it { should be_valid }
+    end
+  end
 end
