c15c5a47c00453604a51233e806d1b10c13f522d
Jason Swett <jason@benfranklinlabs.com>
2018-11-27 14:23:00 -0500

    Add feature spec for episode creation on show creation.

--- spec/features/create_show_spec.rb
+++ spec/features/create_show_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature 'Create show', type: :feature do
-  scenario 'Creating a show' do
+  before do
     visit new_show_path
     fill_in 'Name', with: 'The Ruby Testing Podcast'
     fill_in 'XML feed URL', with: 'https://rss.simplecast.com/podcasts/5739/rss'
     click_on 'Create Show'
+  end
+
+  scenario 'Show creation' do
     expect(page).to have_content('Show was successfully created.')
+  end
+
+  scenario 'Episode creation' do
+    visit episodes_path
+    expect(page).to have_content('How Todd Siegel Got Started with Testing')
   end
 end
